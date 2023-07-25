21a5c66fc5f56c05337708f9c6f158e27bff2e1b
rufdimitri <ruf.dimitri.de@gmail.com>
2023-07-25 14:33:19 +0200

    format text and code

--- src/rd/electrischeBerechnung/ElectrischeBerechnung.java
+++ src/rd/electrischeBerechnung/ElectrischeBerechnung.java
@@ -6,7 +6,7 @@ public class ElectrischeBerechnung {
     public static void main(String[] args) {
         String eingabe;
         eingabe = JOptionPane.showInputDialog(null,
-                "Rechentyp eingeben (1 für Reihenschaltung zweier Widerstände; \n2 für Parallelschaltung zweier Widerstände)");
+                "Rechentyp eingeben:\n1 für Reihenschaltung zweier Widerstände. \n2 für Parallelschaltung zweier Widerstände.");
         int rechnenTyp = Integer.parseInt(eingabe);
 
         eingabe = JOptionPane.showInputDialog(null, "Spannung (V) eingeben: ");
@@ -27,7 +27,8 @@ public class ElectrischeBerechnung {
             double Pg = U * Ig;
 
             JOptionPane.showMessageDialog(null, String.format(
-                    "Berechnete Werte: Re=%.2f\u03A9, Ig=%.2fA, U1=%.2fV, U2=%.2fV, Pg=%.2fW", Re, Ig, U1, U2, Pg));
+                    "Berechnete Werte: \nRe=%.2f\u03A9, \nIg=%.2fA, \nU1=%.2fV, \nU2=%.2fV, \nPg=%.2fW",
+                    Re, Ig, U1, U2, Pg));
         } else if (rechnenTyp == 2) {
             // Parallelschaltung zweier Widerstände
             double Re = R1 * R2 / (R1 + R2); // Ohm
@@ -36,9 +37,9 @@ public class ElectrischeBerechnung {
             double I2 = U / R2; // (in Ohm Ω);
             double Pg = U * Ig; // (in Watt W);
 
-            JOptionPane.showMessageDialog(null,
-                    String.format("Berechnete Werte: Re=%.2f\u03A9, Ig=%.2fA, I1=%.2f\u03A9, I2=%.2f\u03A9, Pg=%.2fW",
-                            Re, Ig, I1, I2, Pg));
+            JOptionPane.showMessageDialog(null, String.format(
+                    "Berechnete Werte: \nRe=%.2f\u03A9, \nIg=%.2fA, \nI1=%.2f\u03A9, \nI2=%.2f\u03A9, \nPg=%.2fW",
+                    Re, Ig, I1, I2, Pg));
         } else {
             JOptionPane.showMessageDialog(null, "Eingegebenen Rechentyp ist nicht bekannt: " + rechnenTyp);
         }
