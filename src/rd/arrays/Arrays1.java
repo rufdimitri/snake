6e24b1f0361b15ded29d1b1077cf62569011c0c5
rufdimitri <ruf.dimitri.de@gmail.com>
2023-07-26 10:30:23 +0200

    create Arrays1

---
+++ src/rd/arrays/Arrays1.java
@@ -0,0 +1,26 @@
+package rd.arrays;
+
+public class Arrays1 {
+    public static void main(String[] args) {
+        int[] array1 = new int[5];
+        array1[0] = 10;
+        array1[1] = 20;
+        array1[2] = 30;
+        array1[3] = 40;
+        array1[4] = 50;
+
+        System.out.print("array1: ");
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + ", ");
+        }
+        System.out.println();
+
+        // array2
+        int[] array2 = { 90, 80, 70, 60 };
+        System.out.print("array2: ");
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i] + ", ");
+        }
+        System.out.println();
+    }
+}
