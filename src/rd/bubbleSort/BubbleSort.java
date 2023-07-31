908c54a1b5ac63e81a14af69bd537e21923b7528
rufdimitri <ruf.dimitri.de@gmail.com>
2023-07-31 11:50:02 +0200

    add Implementations class
    and override sort() method for different implementations
    of Bubble sorting algorithm

--- src/rd/bubbleSort/BubbleSort.java
+++ src/rd/bubbleSort/BubbleSort.java
@@ -1,9 +1,113 @@
 package rd.bubbleSort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class BubbleSort {
+    static class Implementation {
+        static List<Implementation> implementations = new ArrayList<>();
+        private String name;
+        private String description;
+
+        static {
+            implementations.add(new Implementation("Bubble Sort", "Standard implementation, no optimizations") {
+                public void sort(int[] array) {
+                    int buffer;
+                    for (int i = 0; i < array.length; i++) {
+                        for (int j = 0; j < array.length - 1; j++) {
+                            if (array[j] > array[j + 1]) {
+                                buffer = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = buffer;
+                            }
+                        }
+                    }
+                }
+            });
+
+            implementations.add(new Implementation("BS Optimize #1", "Break if no changes to array were made") {
+                public void sort(int[] array) {
+                    int buffer;
+                    boolean changed = true;
+                    while (changed) {
+                        changed = false;
+                        for (int j = 0; j < array.length - 1; j++) {
+                            if (array[j] > array[j + 1]) {
+                                buffer = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = buffer;
+                                changed = true;
+                            }
+                        }
+                    }
+                }
+            });
+
+            implementations.add(new Implementation("BS Optimize #2",
+                    "Loop only up to last sorted number (after every iteration"
+                            + " the biggest number is moved to the end and we dont need to compare it in next iterations)") {
+                public void sort(int[] array) {
+                    int buffer;
+                    for (int i = 0; i < array.length; i++) {
+                        for (int j = 0; j < array.length - 1 - i; j++) {
+                            if (array[j] > array[j + 1]) {
+                                buffer = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = buffer;
+                            }
+                        }
+                    }
+                }
+            });
+
+            implementations.add(new Implementation("BS Optimize #1 & #2",
+                    "Optimization #1: Break if no changes to array were made \n" +
+                            "Optimization #2: Loop only up to last sorted number (after every iteration " +
+                            " the biggest number is moved to the end and we dont need to compare it in next" + //
+                            " iterations)") {
+
+                public void sort(int[] array) {
+                    int buffer;
+                    boolean changed = true;
+                    for (int i = 0; i < array.length; i++) {
+                        changed = false;
+                        for (int j = 0; j < array.length - 1 - i; j++) {
+                            if (array[j] > array[j + 1]) {
+                                buffer = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = buffer;
+                                changed = true;
+                            }
+                        }
+                        if (!changed) {
+                            break;
+                        }
+                    }
+                }
+            });
+
+        }
+
+        Implementation(String name) {
+            this.name = name;
+        }
+
+        Implementation(String name, String description) {
+            this.name = name;
+            this.description = description;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void sort(int[] array) {
+            throw new RuntimeException("Method is not implemented.");
+        };
+
+    }
 
     public static void main(String[] args) {
         testInt(5, 0, 1000);
@@ -24,81 +128,6 @@ public class BubbleSort {
         }
     }
 
-    static void bubbleSort(int[] array) {
-        int buffer;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    buffer = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = buffer;
-                }
-            }
-        }
-    }
-
-    /**
-     * Break if no changes to array were made
-     */
-    static void bubbleSortOpmimize1(int[] array) {
-        int buffer;
-        boolean changed = true;
-        while (changed) {
-            changed = false;
-            for (int j = 0; j < array.length - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    buffer = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = buffer;
-                    changed = true;
-                }
-            }
-        }
-    }
-
-    /**
-     * Loop only up to last sorted number (after every iteration the biggest number
-     * is moved to the end and we dont need to compare it in next iterations)
-     */
-    static void bubbleSortOpmimize2(int[] array) {
-        int buffer;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    buffer = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = buffer;
-                }
-            }
-        }
-    }
-
-    /**
-     * Optimization #1: Break if no changes to array were made
-     * 
-     * Optimization #2: Loop only up to last sorted number (after every iteration
-     * the biggest number is moved to the end and we dont need to compare it in next
-     * iterations)
-     */
-    static void bubbleSortOpmimize1x2(int[] array) {
-        int buffer;
-        boolean changed = true;
-        for (int i = 0; i < array.length; i++) {
-            changed = false;
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    buffer = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = buffer;
-                    changed = true;
-                }
-            }
-            if (!changed) {
-                break;
-            }
-        }
-    }
-
     static void testInt(int length, int valuesFrom, int valuesTo) {
         int[] intArray = {};
         int[] testArray = {};
@@ -106,49 +135,29 @@ public class BubbleSort {
 
         int[] arrayLengthsTestValues = { 1000, 10000, 50000, 60000, 70000, 80000, 90000, 100000, 110000, 120000, 130000,
                 140000, 150000, 160000, 170000 };
-        // TODO: replace testNames and in if (r == 0) { print("Bubble Sort")} else {
-        // bubbleSort(); }
-        // (in if make output and also the calc the values, so that it is better to
-        // understand and call the right function)
-        // String[] testNames = { "Bubble Sort (BS)", "BS Opt #1", "BS Opt #2", "BS Opt
-        // #1 & #2" };
+
         for (int r = 0; r < arrayLengthsTestValues.length + 1; r++) { // +1 for first row with captions
             if (r > 0) {
                 intArray = generateArray(arrayLengthsTestValues[r - 1], valuesFrom, valuesTo);
             }
-            for (int c = 0; c < testNames.length + 1; c++) { // +1 for first column with arrayLengths
+            for (int c = 0; c < (Implementation.implementations.size() + 1); c++) { // +1 for first column with
+                                                                                    // arrayLengths
                 if (r == 0 && c == 0) {
                     System.out.format("%20s", "Amount of elements");
-                } else if (r == 0 && c > 0) {
-                    System.out.format("%20s", testNames[c - 1]);
                 } else if (r > 0 && c == 0) {
                     System.out.format("%20d", arrayLengthsTestValues[r - 1]);
-                } else if (c == 1) { // "Bubble Sort (BS)"
-                    testArray = Arrays.copyOf(intArray, intArray.length);
-                    startTime = System.currentTimeMillis();
-                    bubbleSort(testArray);
-                    endTime = System.currentTimeMillis();
-                    System.out.format("%20d", endTime - startTime);
-                } else if (c == 2) { // "BS Opt #1"
-                    testArray = Arrays.copyOf(intArray, intArray.length);
-                    startTime = System.currentTimeMillis();
-                    bubbleSortOpmimize1(testArray);
-                    endTime = System.currentTimeMillis();
-                    System.out.format("%20d", endTime - startTime);
-                } else if (c == 3) { // "BS Opt #2"
-                    testArray = Arrays.copyOf(intArray, intArray.length);
-                    startTime = System.currentTimeMillis();
-                    bubbleSortOpmimize2(testArray);
-                    endTime = System.currentTimeMillis();
-                    System.out.format("%20d", endTime - startTime);
-                } else if (c == 4) { // "BS Opt #1 & #2"
-                    testArray = Arrays.copyOf(intArray, intArray.length);
-                    startTime = System.currentTimeMillis();
-                    bubbleSortOpmimize1x2(testArray);
-                    endTime = System.currentTimeMillis();
-                    System.out.format("%20d", endTime - startTime);
+                } else if (c > 0) {
+                    Implementation impl = Implementation.implementations.get(c - 1);
+                    if (r == 0) {
+                        System.out.format("%20s", impl.getName());
+                    } else {
+                        testArray = Arrays.copyOf(intArray, intArray.length);
+                        startTime = System.currentTimeMillis();
+                        impl.sort(testArray);
+                        endTime = System.currentTimeMillis();
+                        System.out.format("%20d", endTime - startTime);
+                    }
                 }
-                // System.out.print("\t");
             }
             System.out.println();
         }
