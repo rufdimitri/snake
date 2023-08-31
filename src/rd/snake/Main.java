e9d9a96139f3bbb8552a9d406f24bbc8b0451eff
rufdimitri <ruf.dimitri.de@gmail.com>
2023-08-31 10:54:11 +0200

    refactor: use lamba for Runnable

--- src/rd/snake/Main.java
+++ src/rd/snake/Main.java
@@ -6,13 +6,7 @@ public class Main {
 	static GUIFrame frame;
 
 	public static void main(String[] args) throws InterruptedException {
-		Runnable guiTask = new Runnable() {
-			@Override
-			public void run() {
-				frame = new GUIFrame(300, 300);
-			}
-		};
-
+		Runnable guiTask = () -> frame = new GUIFrame(300, 300);
 		SwingUtilities.invokeLater(guiTask);
 	}
 
