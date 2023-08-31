fa203a4176eb657ccaeb5bbccb8c8b9f3033981c
rufdimitri <ruf.dimitri.de@gmail.com>
2023-08-31 14:01:08 +0200

    Snake structure

--- src/rd/snake/Snake.java
+++ src/rd/snake/Snake.java
@@ -1,12 +1,28 @@
 package rd.snake;
 
 import java.awt.geom.Point2D;
+import java.util.LinkedList;
 
 public class Snake {
+	private LinkedList<Segment> segments = new LinkedList<>();
 
 	public static class Segment {
-		public double direction; // angle in radian
 		public Point2D.Double position;
+		public double rotation; // angle in radian
+
+		public Segment(Point2D.Double position, double rotation) {
+			super();
+			this.position = position;
+			this.rotation = rotation;
+		}
+	}
+
+	public LinkedList<Segment> getSegments() {
+		return segments;
+	}
+
+	public Segment getHead() {
+		return segments.getFirst();
 	}
 
 }
