f31d7aee410ce0e1e12112418f4e6bc8b2ac4ef5
rufdimitri <ruf.dimitri.de@gmail.com>
2023-09-14 15:05:57 +0200

    Food class and createRandomFood(), addRandomFood(int count)

--- src/rd/snake/Food.java
+++ src/rd/snake/Food.java
@@ -5,6 +5,8 @@ import java.awt.geom.Point2D;
 public class Food {
 	Point2D.Double location;
 	int energy;
+	public static final int ENERGY_MIN = 10;
+	public static final int ENERGY_MAX = 100;
 
 	public Food(Point2D.Double location, int energy) {
 		super();
--- src/rd/snake/World.java
+++ src/rd/snake/World.java
@@ -1,14 +1,17 @@
 package rd.snake;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  *  Contains all entities and methods to manipulate them
  */
 public class World {
+	private static Random random = new Random();
 	public Snake snake;
-	public List<Food> food;
+	public List<Food> food = new ArrayList<>();
 	public int width, height;
 	public Point2D.Double center;
 
@@ -24,5 +27,19 @@ public class World {
 		snake.getSegments().add(new Snake.Segment(snake, center, 0)); // manually add head in center of World
 		snake.segmentSize = 20;
 		snake.addSegments(10); // add other segments automatically
+	}
+
+	public void addRandomFood(int count) {
+		for (int i = 0; i < count; i++) {
+			this.food.add(createRandomFood());
+		}
+	}
+
+	public Food createRandomFood() {
+		int energy = random.nextInt(Food.ENERGY_MIN, Food.ENERGY_MAX);
+		double x = random.nextDouble(width);
+		double y = random.nextDouble(height);
+		Point2D.Double location = new Point2D.Double(x, y);
+		return new Food(location, energy);
 	}
 }
