5fb6a20a35e97fb32707be12a86a937b339aec67
rufdimitri <ruf.dimitri.de@gmail.com>
2023-09-14 13:39:40 +0200

    refactor delete unused attribut

--- src/rd/snake/Main.java
+++ src/rd/snake/Main.java
@@ -5,10 +5,9 @@ import java.awt.Color;
 import javax.swing.SwingUtilities;
 
 public class Main {
-	static GUIFrame frame;
 
 	public static void main(String[] args) throws InterruptedException {
-		Runnable guiTask = () -> frame = new GUIFrame(500, 500, Color.WHITE);
+		Runnable guiTask = () -> new GUIFrame(500, 500, Color.WHITE);
 		SwingUtilities.invokeLater(guiTask);
 	}
 
